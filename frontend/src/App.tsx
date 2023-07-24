38560e12761111115461d708b09abeb3cc7518ae
Alex Lujan <alexlujanm@outlook.com>
2023-07-24 00:19:28 -0600

    Created Card component and document icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import Card from "./components/Card"
+
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div >
+      <Card commitHash="60fffa54" commitTitle="Add the social media plugin on iOS app" commitUser={
+        { userImageUrl: "https://avatars.githubusercontent.com/u/55332181?v=4", username: "AlexLm" }
+      } date="7/23/23" filesCommited={3} />
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,47 @@
+interface CardProps {
+  date: string;
+  commitHash: string;
+  commitTitle: string;
+  commitUser: {
+    username: string;
+    userImageUrl: string;
+  };
+  filesCommited: number
+}
+import filesimg from '../assets/document-icon.svg'
+const Card = ({ date, commitHash, commitTitle, commitUser, filesCommited }: CardProps) => {
+  return (
+    <article className="p-4  bg-gradient-to-r from-purple-600 to-blue-600 rounded-lg max-w-sm">
+      <div className='flex  max-w-sm flex-col gap-3'>
+
+        <div id="commit-date-hash-container" className="flex justify-between w-full">
+          <div id="commit-date">
+            <p className="text-white font-medium">{date}</p>
+          </div>
+          <div id="commit-hash">
+            <p className="text-white font-medium">{commitHash}</p>
+          </div>
+        </div>
+        <div id="commit-title-files-container" className="flex flex-col">
+          <div id="commit-title" className="text-white w-full mb-1 font-medium">
+            {commitTitle}
+          </div>
+          <div id="commit-files-commited" className="flex items-center">
+            <img src={filesimg} alt="images-file-logo" className='mr-3' />
+            <p className="text-gray-400 font-medium">{filesCommited} files commited</p>
+          </div>
+        </div>
+        <div id="commit-image-user-container" className="flex items-center" >
+          <img src={commitUser.userImageUrl} alt="userImage" className="w-8 h-8 rounded-full mr-2" />
+
+          <div id="commit-username">
+            <p className='text-gray-300'>{commitUser.username}</p>
+          </div>
+        </div>
+      </div>
+
+    </article>
+  )
+}
+
+export default Card
