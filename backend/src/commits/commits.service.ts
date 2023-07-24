d6c63649539182ba094f7bbe9dd774ac4f7e90cb
Alex Lujan <alexlujanm@outlook.com>
2023-07-24 15:46:35 -0600

    Changed getCommits function to map only the required data and the additional data

--- backend/src/commits/commits.service.ts
+++ backend/src/commits/commits.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { OctokitService } from 'nestjs-octokit';
 import { GithubService } from 'src/github/github.service';
+import { Interfaces } from '../common/interfaces';
 
 @Injectable()
 export class CommitsService {
@@ -8,9 +8,33 @@ export class CommitsService {
 
   async getCommits() {
     try {
-        const response = await this.githubService.getCommitsFromRepo();
+      const response: Interfaces.FullCommitStructure[] =
+        await this.githubService.getCommitsFromRepo();
 
-        return response;
+      let mappedCommits: Interfaces.CustomCommit[] = [];
+
+      if (response.length > 0) {
+        mappedCommits = await Promise.all(
+          response.map(async (commit) => {
+            const customCommit: Interfaces.CustomCommit = {
+              sha: commit.sha,
+              message: commit.commit.message,
+              username: commit.author.login,
+              date: commit.commit.committer.date,
+              avatar: commit.committer.avatar_url,
+              files: 0,
+            };
+
+            const { files } = await this.githubService.getCommitByRef(customCommit.sha);
+
+            customCommit.files = files.length;
+
+            return customCommit;
+          }),
+        );
+      }
+
+      return mappedCommits;
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
