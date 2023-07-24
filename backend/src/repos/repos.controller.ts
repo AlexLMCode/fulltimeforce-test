3be468d1aa853514bfa2d7790d9f240d243a3d23
Alex Lujan <alexlujanm@outlook.com>
2023-07-24 16:58:02 -0600

    Added controller types

--- backend/src/repos/repos.controller.ts
+++ backend/src/repos/repos.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { ReposService } from './repos.service';
+import { Interfaces } from 'src/common/interfaces';
 
 @Controller('repos')
 export class ReposController {
     constructor(private repoService: ReposService) {}
 
     @Get('/info')
-    async getRepoInfo() {
+    async getRepoInfo(): Promise<Interfaces.RepositoryInfo> {
         return this.repoService.getRepoInfo();
     }
 }
