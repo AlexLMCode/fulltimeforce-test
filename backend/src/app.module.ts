2a323145dfb4965951097eceaf21ce4441ddce93
Alex Lujan <alexlujanm@outlook.com>
2023-07-24 08:42:41 -0600

    Added config module globally

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Environment } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: Environment.EnvValidation.validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
