7521c2478468a991f43f9829dbbd2e10ea434904
Alex Lujan <alexlujanm@outlook.com>
2023-07-24 20:40:55 -0600

    Swagger implemented in Commits module

--- backend/src/commits/commits.controller.ts
+++ backend/src/commits/commits.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
 import { CommitsService } from './commits.service';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Commits')
 @Controller('commits')
 export class CommitsController {
     constructor(private commitsService: CommitsService) {}
 
+    @ApiQuery({ name:'page', required: true, type: Number })
+    @ApiQuery({ name:'perPage', required: true, type: Number })
     @HttpCode(HttpStatus.OK)
     @Get()
     async getCommits(@Query('page') page: number, @Query('perPage') perPage: number) {
