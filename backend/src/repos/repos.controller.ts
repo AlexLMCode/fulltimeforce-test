6d25b1fc8324fbc2159420407e777b36e84765eb
Alex Lujan <alexlujanm@outlook.com>
2023-07-24 20:41:03 -0600

    Swagger implemented in Repos module

--- backend/src/repos/repos.controller.ts
+++ backend/src/repos/repos.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, HttpCode } from '@nestjs/common';
 import { ReposService } from './repos.service';
 import { Interfaces } from 'src/common/interfaces';
+import { ApiTags } from '@nestjs/swagger';
 
+ @ApiTags('Repos')
 @Controller('repos')
 export class ReposController {
     constructor(private repoService: ReposService) {}
 
+    @HttpCode(HttpStatus.OK)
     @Get('/info')
     async getRepoInfo(): Promise<Interfaces.RepositoryInfo> {
         return this.repoService.getRepoInfo();
