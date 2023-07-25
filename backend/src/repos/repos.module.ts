06b83a51566e0b465e0e96a0ad256e96a003048b
Alex Lujan <alexlujanm@outlook.com>
2023-07-24 19:48:21 -0600

    Added date formatter to get repo info service

--- backend/src/repos/repos.module.ts
+++ backend/src/repos/repos.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReposController } from './repos.controller';
 import { ReposService } from './repos.service';
 import { GithubModule } from 'src/github/github.module';
+import { DateFormatterModule } from 'src/date-formatter/date-formatter.module';
 
 @Module({
   controllers: [ReposController],
   providers: [ReposService],
-  imports: [GithubModule]
+  imports: [GithubModule, DateFormatterModule]
 })
 export class ReposModule {}
